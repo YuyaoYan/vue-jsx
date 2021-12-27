2ee363f1fe8debf1d7208daec4bf027a21632d0c
闫煜瑶 <y.yy@winning.com.cn>
2021-12-27 09:43:37 +0800

    first commit

---
+++ src/components/ChristmasTree.vue
@@ -0,0 +1,33 @@
+
+<script>
+import "./tree.css";
+export default {
+  data() {
+    return {};
+  },
+  render() {
+    const len = [new Array(3), new Array(4)];
+    let item = len.map((i) => {
+      let line = i.map(() => {
+        return (
+          <p
+            style={
+              "width:32px;height: 32px;background: green;display:inline-block;margin-right:2px"
+            }
+            className={"leaf"}
+          ></p>
+        );
+      });
+      return line;
+    });
+    return <div className={"tree"}>{item}</div>;
+  },
+};
+</script>
+<style lang="css">
+.leaf {
+  width: 20px;
+  height: 20px;
+  background: green;
+}
+</style>
